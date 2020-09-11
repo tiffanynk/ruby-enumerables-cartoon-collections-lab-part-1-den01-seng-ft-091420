0465b134cb0dc0785ca1d9b2a8dbf4425a16ff2d
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-11 19:25:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
 def greet_characters(array)
   # Use `each` to enumerate over the provided array
-  #
+  array.each do |element|
+    puts "Hello, #{element.capitalize}!"
+  end
   # Print a custom greeting for each element
 end
 
